6edf36d953859d649ce3f1401e9d9943c7bbae54
iamsourav37 <souravganguly108@gmail.com>
2021-04-10 19:26:10 +0530

    todo list using useState hooks

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../components/todo_list_style.css";
 
 const TodoList = () => {
+  const [myTodos, setTodo] = useState([]);
+  const [inputData, setInputData] = useState("");
+
+  const addTodo = (event) => {
+    console.log("in addTodo function :", inputData);
+    if (inputData !== "") setTodo((oldData) => [...oldData, inputData]);
+    setInputData("");
+  };
+
+  const inputDataOnChange = (event) => {
+    setInputData(event.target.value);
+  };
+
   return (
     <div className="main_div">
       <div className="center_div">
         <br />
         <h1>TodoList</h1>
         <br />
-        <input type="text" placeholder="Add a Todo" />
-        <button>+</button>
+        <input
+          type="text"
+          placeholder="Add a Todo"
+          onChange={inputDataOnChange}
+          value={inputData}
+        />
+        <button onClick={addTodo}>+</button>
 
         <ol>
-          <li>learn React</li>
+          {myTodos.map((value, index) => {
+            return <li key={index}>{value}</li>;
+          })}
         </ol>
       </div>
     </div>
